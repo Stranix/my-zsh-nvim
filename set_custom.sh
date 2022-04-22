67f17b57b00f7e2bf6ff392cc8cae240ee4ba792
Konstantin <stranix05@inbox.ru>
2022-04-22 11:17:25 +0300

    Fix

--- set_custom.sh
+++ set_custom.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-echo "Кастомизируем"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+# Настраиваем nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-cp -f coc-settings.json ~/.config/nvim/
-cp -f init.vim ~/.config/nvim/
-cp -f .tmux.conf ~/
-cp -f .zshrc ~/
-echo "Меняем оболочку на zsh"
-chsh -s /usr/bin/zsh
+# окружение питона для подсказок
+python3 -m venv ~/.config/nvim/env
+source ~/.config/nvim/env/bin/activate
+pip install wheel
+pip install yapf doq
+deactivate
+#украшательство nvim
+sudo apt install -y silversearcher-ag
+wget https://github.com/sharkdp/bat/releases/download/v0.15.4/bat_0.15.4_amd64.deb
+sudo dpkg -i ./bat_0.15.4_amd64.deb
+sudo apt install -y -o Dpkg::Options::="--force-overwrite" bat ripgrep
+#настраиваем zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
