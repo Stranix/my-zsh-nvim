4e160563dab6966a18c47db1c305a2c40817518f
Konstantin <stranix05@inbox.ru>
2022-04-22 10:07:42 +0300

    Fix sh script and add fonts

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo add-apt-repository ppa:deadsnakes/ppa
 sudo add-apt-repository ppa:neovim-ppa/stable
 echo "Обновляем"
 sudo apt update
+sudo apt install zsh tmux htop -y
 echo "Закачиваем нужные скрипты и пакеты"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -15,7 +16,6 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 wget https://github.com/sharkdp/bat/releases/download/v0.15.4/bat_0.15.4_amd64.deb
 source ~/.bashrc
 echo "Подготовка среды"
-sudo apt install zsh tmux htop -y
 sudo apt install python-dev python-pip python3-dev python3-pip -y
 sudo apt install python3.10 -y
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
@@ -36,7 +36,7 @@ source ~/.config/nvim/env/bin/activate
 pip install wheel
 pip install yapf doq
 deactivate
-cp -f oc-settings.json ~/.config/nvim/
+cp -f coc-settings.json ~/.config/nvim/
 cp -f init.vim ~/.config/nvim/
 cp -f .tmux.conf ~/
 cp -f .zshrc ~/
